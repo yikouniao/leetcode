8ec312706f3af3b9f0501edec49ddfe02aaa5e40
Yikouniao <chinoandtippi@gmail.com>
2022-04-11 19:26:39 +0800

    Update 0027-remove-element.py

--- 0027-remove-element.py
+++ 0027-remove-element.py
@@ -4,20 +4,14 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i, j = 0, len(nums) - 1
-        while i <= j:
-            if nums[j] == val:
-                j -= 1
-            elif nums[i] == val:
-                nums[i] = nums[j]
-                nums[j] = val
-                j -= 1
-                i += 1
-            else:
-                i += 1
-        return j + 1
-        
-        
+        n = len(nums)
+        p1, p2 = n - 1, n - 1
+        while p1 >= 0:
+            if nums[p1] == val:
+                nums[p1] = nums[p2]
+                p2 -= 1
+            p1 -= 1
+        return p2 + 1
 
 
 if __name__ == '__main__':
